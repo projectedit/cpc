f35159198213599a9b8666732eab0e4f15700e6b
projectedit <admin@projectedit.co.za>
2023-09-15 15:50:10 +0200

    add menu and gallary need finalization for final upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { GallaryComponent } from './pages/gallary/gallary.component';
+import { FaqComponent } from './pages/faq/faq.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent },
+  {path:'home', component:HomeComponent },
+  {path: 'menu', component:MenuComponent},
+  {path: 'gallary', component: GallaryComponent},
+  {path: 'faq', component: FaqComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { GallaryComponent } from './pages/gallary/gallary.component';
+import { FaqComponent } from './pages/faq/faq.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    MenuComponent,
+    GallaryComponent,
+    FaqComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
